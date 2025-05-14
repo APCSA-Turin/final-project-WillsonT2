0792938ea14613d32b9e8b3656ba805868f607a4
Willson <180681710+WillsonT2@users.noreply.github.com>
2025-05-14 11:30:12 -0400

    test

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame f=new JFrame();//creating instance of JFrame
+        JButton b=new JButton("Play");//creating instance of JButton
+        b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+        f.add(b);//adding button in JFrame
+        f.setSize(400,500);//400 width and 500 height
+        f.setLayout(null);//using no layout managers
+        f.setVisible(true);//making the frame visible
+    }
+}
