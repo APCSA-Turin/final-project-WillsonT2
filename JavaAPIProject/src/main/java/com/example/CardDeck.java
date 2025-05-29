91ec486f298bb79de83072fc863f17a40091f523
Willson <--global>
2025-05-29 01:22:17 -0400

    grape

--- JavaAPIProject/src/main/java/com/example/CardDeck.java
+++ JavaAPIProject/src/main/java/com/example/CardDeck.java
@@ -1,44 +1,36 @@
 package com.example;
 
-import java.io.IOException;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 
 public class CardDeck {
-    private HashMap<Card, Integer> fullDeck;
+    private HashMap<String, Integer> fullDeck;
 
     public CardDeck(){
         HashMap<String, Card> tempCards = CardLoader.getAllCards();
         Collection<Card> values = tempCards.values();
+        fullDeck = new HashMap<>();
         for (Card value : values){
             if (value.getTags().contains("basic")){
-                fullDeck.put(value, fullDeck.getOrDefault(value, 0) + 1);
-                if (value.getTags().contains("strike") || value.getTags().contains("defend")){
-                    fullDeck.put(value, fullDeck.getOrDefault(value, 0) + 1);
-                    fullDeck.put(value, fullDeck.getOrDefault(value, 0) + 1);
-                    fullDeck.put(value, fullDeck.getOrDefault(value, 0) + 1);
-                }
+                int bonus = (value.getTags().contains("strike")||value.getTags().contains("defend")) ? 3 : 0;
+                fullDeck.put(value.getId(), fullDeck.getOrDefault(value.getId(),0) + 1 + bonus);
             }
         }
     }
 
-    public void removeCard(Card removeCard){
-        if (fullDeck.get(removeCard) == 1){
-            fullDeck.remove(removeCard);
+    public void removeCard(String cardId){
+        if (fullDeck.get(cardId) == null){
+            return;
+        }else if (fullDeck.get(cardId) == 1){
+            fullDeck.remove(cardId);
         }else {
-            fullDeck.put(removeCard, fullDeck.get(removeCard) - 1);
+            fullDeck.put(cardId, fullDeck.get(cardId) - 1);
         }
     }
 
     public void addCard(Card addCard){
-        fullDeck.put(addCard, fullDeck.getOrDefault(addCard, 0) + 1);
+        fullDeck.put(addCard.getId(), fullDeck.getOrDefault(addCard.getId(), 0) + 1);
     }
 
-    public void upgradeCard(Card upgradeCard){
-        Card upgradedCard = new Card(upgradeCard.getCardDefinition());
-        upgradedCard.upgradeCard();
-        fullDeck.put(upgradedCard, fullDeck.getOrDefault(upgradedCard, 0) + 1);
-        removeCard(upgradeCard);
-    }
+
 }
